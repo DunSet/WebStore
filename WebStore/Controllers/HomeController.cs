00e48176a9f355b78e7d99cfbdc1cd1b7663e1c9
Yaroslav Afanasiev <afanasiev_y@rusklimat.ru>
2020-05-24 14:07:42 +0300

    Передача данных (модели) в представление

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -3,14 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> __Employees = new List<Employee>
+        {
+            new Employee
+            {
+                Id = 1,
+                Surname = "Родионова",
+                FirstName = "Алена",
+                Patronimic = "Витальевна",
+                Age = 21
+            },
+             new Employee
+            {
+                Id = 2,
+                Surname = "Фельчер",
+                FirstName = "Патрик",
+                Patronimic = "Петрович",
+                Age = 26
+            },
+              new Employee
+            {
+                Id = 3,
+                Surname = "Незабудка",
+                FirstName = "Михаил",
+                Patronimic = "Олегович",
+                Age = 45
+            },
+        };
+       
         public IActionResult Index()
         {
-            return View();
+
+            //ViewBag.Info = "Hello world";
+            ViewData["Info"] = "Hello world!";
+            return View(__Employees);
         }
 
         public IActionResult AnotherAction()
---
+++ WebStore/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Patronimic { get; set; }
+
+        public int Age { get; set; }
+    }
+}
