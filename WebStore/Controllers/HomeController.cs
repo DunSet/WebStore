2f5eabe7a591d504176f2daec758fdfc41fbf219
Yaroslav Afanasiev <afanasiev_y@rusklimat.ru>
2020-05-30 18:44:24 +0300

    Выделяем под управление сотрудниками отдельный контроллер

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -9,48 +9,7 @@ namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        private static readonly List<Employee> __Employees = new List<Employee>
-        {
-            new Employee
-            {
-                Id = 1,
-                Surname = "Родионова",
-                FirstName = "Алена",
-                Patronimic = "Витальевна",
-                Age = 21
-            },
-             new Employee
-            {
-                Id = 2,
-                Surname = "Фельчер",
-                FirstName = "Патрик",
-                Patronimic = "Петрович",
-                Age = 26
-            },
-              new Employee
-            {
-                Id = 3,
-                Surname = "Незабудка",
-                FirstName = "Михаил",
-                Patronimic = "Олегович",
-                Age = 45
-            },
-        };
-
-        public IActionResult Index() => View();
-       
-
-        public IActionResult EmployeeDetails(int id)
-        {
-            var employee = __Employees.FirstOrDefault(e => e.Id == id);
-            if (employee is null)
-                return NotFound();
-
-            return View(employee);
-        }
-        public IActionResult AnotherAction()
-        {
-            return Content("Another action result");
-        }
+      public IActionResult Index() => View();
+            
     }
 }
