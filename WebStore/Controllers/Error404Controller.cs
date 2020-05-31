b5d07886a64b4568c6fa081db44862bb0d91a3b0
Yaroslav Afanasiev <afanasiev_y@rusklimat.ru>
2020-05-31 15:06:53 +0300

    Добавление Error controller и представления

---
+++ WebStore/Controllers/Error404Controller.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class Error404Controller : Controller
+    {
+        public IActionResult Index() => View();
+    
+    }
+}
