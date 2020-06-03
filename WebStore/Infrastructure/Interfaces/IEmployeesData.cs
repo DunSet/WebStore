e0eed2294e0b7fdb7961b8b5a79acbd43e937e0c
Yaroslav Afanasiev <afanasiev_y@rusklimat.ru>
2020-06-03 23:00:08 +0300

    Интерфейс сервиса управления сотрудниками

---
+++ WebStore/Infrastructure/Interfaces/IEmployeesData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Models;
+
+namespace WebStore.Infrastructure.Interfaces
+{
+    interface IEmployeesData
+    {
+        IEnumerable<Employee> Get();//reading
+        Employee GetById(int id);
+        int Add(Employee Employee);
+        void Edit(Employee employee);
+        bool Delete(int id);
+        void SaveChanges();
+    }
+}
