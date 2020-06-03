9ae34fef1437cc89cbe54bbcc625712c5f5bb827
Yaroslav Afanasiev <afanasiev_y@rusklimat.ru>
2020-06-03 22:48:23 +0300

    Выносим данные из контроллера

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -3,45 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Data;
 using WebStore.Models;
 
 namespace WebStore.Controllers
 {
-    [Route("Staff")]
+    //[Route("Staff")]
     public class EmployeesController : Controller
     {
-        private static readonly List<Employee> __Employees = new List<Employee>
-        {
-            new Employee
-            {
-                Id = 1,
-                Surname = "Родионова",
-                FirstName = "Алёна",
-                Patronimic = "Валерьевна",
-                Age = 21
-            },
-             new Employee
-            {
-                Id = 2,
-                Surname = "Фельчер",
-                FirstName = "Патрик",
-                Patronimic = "Петрович",
-                Age = 26
-            },
-              new Employee
-            {
-                Id = 3,
-                Surname = "Незабудка",
-                FirstName = "Михаил",
-                Patronimic = "Олегович",
-                Age = 45
-            },
-        };
-
-        [Route("List")]
+        private static readonly List<Employee> __Employees = TestData.Employees;
+     
+    //[Route("List")]
         public IActionResult Index() => View(__Employees);
       
-        [Route("{id}")]
+    //[Route("{id}")]
         public IActionResult EmployeeDetails(int id)
         {
             var employee = __Employees.FirstOrDefault(e => e.Id == id);
