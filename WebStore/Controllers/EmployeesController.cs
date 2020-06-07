03cf1532b067c24e01cf09f66b5f1538e5e5e735
Yaroslav Afanasiev <afanasiev_y@rusklimat.ru>
2020-06-07 16:43:32 +0300

    Редакт данных в контроллере сотрудников

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using WebStore.Data;
 using WebStore.Infrastructure.Interfaces;
 using WebStore.Models;
+using WebStore.ViewModel;
 
 namespace WebStore.Controllers
 {
@@ -32,5 +33,56 @@ namespace WebStore.Controllers
 
             return View(employee);
         }
+
+        #region Редактирование
+
+        public IActionResult Edit(int? Id) //редактирование
+        {
+            if (Id is null) return View(new EmployeeViewModel());
+
+            if (Id < 0)
+                return BadRequest();
+
+            var employee = _EmployeesData.GetById((int) Id);
+            if (employee is null)
+                return NotFound();
+
+            return View(new EmployeeViewModel
+            { 
+                Id = employee.Id,
+                Surname = employee.Surname,
+                Name = employee.FirstName,
+                Patronimic = employee.Patronimic,
+                Age = employee.Age
+
+            
+            });
+        }
+        [HttpPost]
+        public IActionResult Edit(EmployeeViewModel Model) // редирект после редактирвоаиня
+        {
+            if (Model is null)
+                throw new ArgumentNullException(nameof(Model));
+
+
+            var employee = new Employee
+            {
+                Id = Model.Id,
+                FirstName = Model.Name,
+                Surname = Model.Surname,
+                Patronimic = Model.Patronimic,
+                Age = Model.Age
+            };
+            if (Model.Id == 0)
+                _EmployeesData.Add(employee);
+            else
+                _EmployeesData.Edit(employee);
+
+            _EmployeesData.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        #endregion
     }
 }
